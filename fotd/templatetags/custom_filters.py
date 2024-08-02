5cb83e80d197256fed22039457552e61f2ec0939
Frank Fang <14533348@qq.com>
2024-08-02 14:26:23 +0800

    Issue10: use customer filter to add links to PR/feature IDs; for names, use link to copy to clipboard

--- fotd/templatetags/custom_filters.py
+++ fotd/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
+import re
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -21,3 +23,29 @@ def keyvalue(dict, key):
 @register.filter
 def get_previous_end_fb(endfb_changed_items, key):
     return endfb_changed_items.get(key, {}).get('previous', 'blank') if endfb_changed_items else 'blank'
+
+# add link to PRs, CB features, CNI features, and email addresses
+link_patterns = {
+    r'PR\d{6}': 'https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText={}',
+    r'CB\d{6}-[CS]R': '/backlog/{}/',
+    # r'CB\d{4,6}': '/backlog/{}-SR/',
+    r'CNI-\d{6}': '/backlog/{}/',
+    r'\b[A-Z][\w-]+\s(?:[\w-]+\s)*(?:\d{1,2}\.\s)?\w+\s\((Nokia|NSB)\)': 'copy-to-clipboard:{}',
+}
+
+@register.filter
+def linkify(value):
+    def replace_match(match):
+        for pattern, url in link_patterns.items():
+            if re.fullmatch(pattern, match.group(0)):
+                if 'copy-to-clipboard' in url:
+                    name = match.group(0)
+                    return f'<a href="#" onclick="navigator.clipboard.writeText(\'{name}\'); alert(\'Copied: {name}\'); return false;">{name}</a>'
+                else:
+                    return f'<a href="{url.format(match.group(0))}" target="_blank">{match.group(0)}</a>'
+        return match.group(0)
+
+    combined_pattern = '|'.join(f'({pattern})' for pattern in link_patterns.keys())
+    regex = re.compile(combined_pattern)
+    result = regex.sub(replace_match, value)
+    return mark_safe(result)
--- fotd/tests/test_custom_filters.py
+++ fotd/tests/test_custom_filters.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.safestring import SafeString
 from fotd.templatetags import custom_filters
 
 class CustomFiltersTest(TestCase):
@@ -17,3 +18,66 @@ class CustomFiltersTest(TestCase):
 		endfb_changed_items = {'item1': {'previous': 'value1'}}
 		self.assertEqual(custom_filters.get_previous_end_fb(endfb_changed_items, 'item1'), 'value1')
 		self.assertEqual(custom_filters.get_previous_end_fb(endfb_changed_items, 'item2'), 'blank')
+
+class LinkifyPrTests(TestCase):
+    def test_linkify_pr(self):
+        input_text = "Here is a PR number: PR123456, and one more: PR654321."
+        expected_output = (
+            'Here is a PR number: '
+            '<a href="https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText=PR123456" target="_blank">PR123456</a>'
+            ', and one more: '
+            '<a href="https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText=PR654321" target="_blank">PR654321</a>.'
+        )
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
+
+    def test_linkify_feature(self):
+        input_text = "The FOTL is taking care of feature CB123456-CR, CB012842-SR and CNI-654321."
+        expected_output = (
+            'The FOTL is taking care of feature '
+            '<a href="/backlog/CB123456-CR/" target="_blank">CB123456-CR</a>'
+            ', <a href="/backlog/CB012842-SR/" target="_blank">CB012842-SR</a>'
+            ' and <a href="/backlog/CNI-654321/" target="_blank">CNI-654321</a>.'
+        )
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
+
+
+    def test_linkify_normal_name(self):
+        input_text = "Contact: Hao-William Hu (NSB)."
+        expected_output = (
+            'Contact: '
+            '<a href="#" onclick="navigator.clipboard.writeText(\'Hao-William Hu (NSB)\'); alert(\'Copied: Hao-William Hu (NSB)\'); return false;">Hao-William Hu (NSB)</a>.'
+        )
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
+
+    def test_linkify_name_with_middle_name(self):
+        input_text = "Contact: Marc Claramunt Codinas (Nokia)."
+        expected_output = (
+            'Contact: '
+            '<a href="#" onclick="navigator.clipboard.writeText(\'Marc Claramunt Codinas (Nokia)\'); alert(\'Copied: Marc Claramunt Codinas (Nokia)\'); return false;">Marc Claramunt Codinas (Nokia)</a>.'
+        )
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
+
+    def test_linkify_name_with_number(self):
+        input_text = "Contact: Lei 24. Wang (NSB)."
+        expected_output = (
+            'Contact: '
+            '<a href="#" onclick="navigator.clipboard.writeText(\'Lei 24. Wang (NSB)\'); alert(\'Copied: Lei 24. Wang (NSB)\'); return false;">Lei 24. Wang (NSB)</a>.'
+        )
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
+
+    def test_linkify_no_match(self):
+        input_text = "This string does not contain any PR numbers."
+        expected_output = input_text
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
