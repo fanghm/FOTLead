30783604e23dbd331c985f22ec5e5de850573d66
Frank Fang <14533348@qq.com>
2024-08-24 16:33:13 +0800

    Enhance linkify and add tests
    Issue #17

--- fotd/templatetags/custom_filters.py
+++ fotd/templatetags/custom_filters.py
@@ -24,15 +24,22 @@ def keyvalue(dict, key):
 def get_previous_end_fb(endfb_changed_items, key):
     return endfb_changed_items.get(key, {}).get('previous', 'blank') if endfb_changed_items else 'blank'
 
-# add link to PRs, CB features, CNI features, and email addresses
+# add link to PRs, CB features, CNI features, and email addresses, and replace URL as link with truncated text
 link_patterns = {
     r'PR\d{6}': 'https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText={}',
     r'CB\d{6}-[CS]R': '/backlog/{}/',
-    # r'CB\d{4,6}': '/backlog/{}-SR/',
+    r'CB\d{4,6}': '/backlog/{}/',
     r'CNI-\d{6}': '/backlog/{}/',
-    r'\b[A-Z][\w-]+\s(?:[\w-]+\s)*(?:\d{1,2}\.\s)?\w+\s\((Nokia|NSB)\)': 'copy-to-clipboard:{}',
+    #r'\b[A-Z][a-z-]+\s(?:[A-Z][\w\.]+\s)?(?:\d{1,2}\.\s)?[A-Z]\w+\s\((Nokia|NSB)\)': 'copy-to-clipboard:{}', 
+    # outlook name in the form of 'First-name M. Lastname (Nokia|NSB)'
+    r'\b[A-Z][a-z]+(?:-[A-Z][a-z]+)?(?: [A-Z]\.)?(?: \d{1,2}\.)? [A-Z][a-z]+ \((Nokia|NSB)\)': 'copy-to-clipboard:{}',
 }
 
+# to match a URL and also a truncated URL with '...' in the middle
+url_pattern = re.compile(r'https?://[^\s.]+(?:\.[^\s.]+)*(?:/[^\s]*)?(?:\.\.\.[^\s]*)?')
+
+# URL text (http//....) to be converted as html link and the anchor text to be shown as truncated text if it's too long, 
+# while any PR/feature number within the anchor text shouldn't be further transformed
 @register.filter
 def linkify(value):
     def replace_match(match):
@@ -40,12 +47,35 @@ def linkify(value):
             if re.fullmatch(pattern, match.group(0)):
                 if 'copy-to-clipboard' in url:
                     name = match.group(0)
-                    return f'<a href="#" onclick="navigator.clipboard.writeText(\'{name}\'); alert(\'Copied: {name}\'); return false;">{name}</a>'
+                    return f'<a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'{name}\'); alert(\'Name copied: {name}\'); return false;">{name}</a>'
                 else:
+                    if pattern == r'CB\d{4,6}':
+                        cb_number = match.group(0)
+                        cb_number_padded = cb_number[:2] + cb_number[2:].zfill(6) + '-SR'
+                        return f'<a href="{url.format(cb_number_padded)}" target="_blank">{cb_number}</a>'
                     return f'<a href="{url.format(match.group(0))}" target="_blank">{match.group(0)}</a>'
         return match.group(0)
 
+    def truncate_url(match):
+        url = match.group(0)
+        if len(url) > 50:
+            return f'{url[:20]}...{url[-20:]}'
+        return url
+
+    # First, handle URLs and replace them with truncated links
+    result1 = url_pattern.sub(lambda match: f'<a href="{match.group(0)}" target="_blank">{truncate_url(match)}</a>', value)
+
+    # Then, handle PR/feature numbers or names, but skip already processed URLs
+    def replace_non_url(match):
+        if url_pattern.search(match.group(0)):
+            #print(f'{match.group(0)} is a URL')
+            return match.group(0)  # Skip if it's a URL (or a truncated URL) to avoid further transformation
+
+        return replace_match(match)
+
     combined_pattern = '|'.join(f'({pattern})' for pattern in link_patterns.keys())
+    combined_pattern = f'({url_pattern.pattern})|{combined_pattern}'
     regex = re.compile(combined_pattern)
-    result = regex.sub(replace_match, value)
+    result = regex.sub(replace_non_url, result1)
+    
     return mark_safe(result)
--- fotd/tests/test_custom_filters.py
+++ fotd/tests/test_custom_filters.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.utils.safestring import SafeString
+from django.utils.safestring import SafeString, mark_safe
 from fotd.templatetags import custom_filters
 
 class CustomFiltersTest(TestCase):
@@ -19,7 +19,7 @@ class CustomFiltersTest(TestCase):
 		self.assertEqual(custom_filters.get_previous_end_fb(endfb_changed_items, 'item1'), 'value1')
 		self.assertEqual(custom_filters.get_previous_end_fb(endfb_changed_items, 'item2'), 'blank')
 
-class LinkifyPrTests(TestCase):
+class LinkifyTests(TestCase):
     def test_linkify_pr(self):
         input_text = "Here is a PR number: PR123456, and one more: PR654321."
         expected_output = (
@@ -32,48 +32,94 @@ class LinkifyPrTests(TestCase):
         self.assertIsInstance(result, SafeString)
         self.assertEqual(result, expected_output)
 
-    def test_linkify_feature(self):
-        input_text = "The FOTL is taking care of feature CB123456-CR, CB012842-SR and CNI-654321."
+    def test_linkify_feature_id(self):
+        input_text = "The FOTL is taking care of feature CB8701, CB123456, CB012842-CR and CNI-654321."
         expected_output = (
             'The FOTL is taking care of feature '
-            '<a href="/backlog/CB123456-CR/" target="_blank">CB123456-CR</a>'
-            ', <a href="/backlog/CB012842-SR/" target="_blank">CB012842-SR</a>'
+            '<a href="/backlog/CB008701-SR/" target="_blank">CB8701</a>'
+            ', <a href="/backlog/CB123456-SR/" target="_blank">CB123456</a>'
+            ', <a href="/backlog/CB012842-CR/" target="_blank">CB012842-CR</a>'
             ' and <a href="/backlog/CNI-654321/" target="_blank">CNI-654321</a>.'
         )
         result = custom_filters.linkify(input_text)
         self.assertIsInstance(result, SafeString)
         self.assertEqual(result, expected_output)
 
-
     def test_linkify_normal_name(self):
-        input_text = "Contact: Hao-William Hu (NSB)."
+        input_text = "Waiting feedback from Wei-William Lee (NSB)."
         expected_output = (
-            'Contact: '
-            '<a href="#" onclick="navigator.clipboard.writeText(\'Hao-William Hu (NSB)\'); alert(\'Copied: Hao-William Hu (NSB)\'); return false;">Hao-William Hu (NSB)</a>.'
+            'Waiting feedback from '
+            '<a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'Wei-William Lee (NSB)\'); alert(\'Name copied: Wei-William Lee (NSB)\'); return false;">Wei-William Lee (NSB)</a>.'
+        )
+        result = custom_filters.linkify(input_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_output)
+
+    def test_linkify_normal_name_and_feature_id(self):
+        input_text = "Waiting feedback from CB9999 FOTL Frankson Smith (Nokia)"
+        expected_output = (
+            'Waiting feedback from '
+            '<a href="/backlog/CB009999-SR/" target="_blank">CB9999</a> FOTL <a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'Frankson Smith (Nokia)\'); alert(\'Name copied: Frankson Smith (Nokia)\'); return false;">Frankson Smith (Nokia)</a>'
         )
         result = custom_filters.linkify(input_text)
         self.assertIsInstance(result, SafeString)
         self.assertEqual(result, expected_output)
 
     def test_linkify_name_with_middle_name(self):
-        input_text = "Contact: Marc Claramunt Codinas (Nokia)."
+        input_text = "Contact: Mask M. Mike (Nokia)."
         expected_output = (
             'Contact: '
-            '<a href="#" onclick="navigator.clipboard.writeText(\'Marc Claramunt Codinas (Nokia)\'); alert(\'Copied: Marc Claramunt Codinas (Nokia)\'); return false;">Marc Claramunt Codinas (Nokia)</a>.'
+            '<a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'Mask M. Mike (Nokia)\'); alert(\'Name copied: Mask M. Mike (Nokia)\'); return false;">Mask M. Mike (Nokia)</a>.'
         )
         result = custom_filters.linkify(input_text)
         self.assertIsInstance(result, SafeString)
         self.assertEqual(result, expected_output)
 
     def test_linkify_name_with_number(self):
-        input_text = "Contact: Lei 24. Wang (NSB)."
+        input_text = "Contact: Hua 77. Zhao (NSB)."
         expected_output = (
             'Contact: '
-            '<a href="#" onclick="navigator.clipboard.writeText(\'Lei 24. Wang (NSB)\'); alert(\'Copied: Lei 24. Wang (NSB)\'); return false;">Lei 24. Wang (NSB)</a>.'
+            '<a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'Hua 77. Zhao (NSB)\'); alert(\'Name copied: Hua 77. Zhao (NSB)\'); return false;">Hua 77. Zhao (NSB)</a>.'
         )
         result = custom_filters.linkify(input_text)
         self.assertIsInstance(result, SafeString)
         self.assertEqual(result, expected_output)
+
+    def test_linkify_short_url(self):
+        self.assertEqual(
+            custom_filters.linkify('This is a short URL: https://example.com/short.'),
+            mark_safe('This is a short URL: <a href="https://example.com/short" target="_blank">https://example.com/short</a>.')
+        )
+
+    def test_linkify_short_url_with_feature_id(self):
+        self.assertEqual(
+            custom_filters.linkify('This is a short URL: http://example.com/short/feature/CB11098/.'),
+            mark_safe('This is a short URL: <a href="http://example.com/short/feature/CB11098/" target="_blank">http://example.com/short/feature/CB11098/</a>.')
+        )
+
+    def test_linkify_short_url_with_pr_id(self):
+        self.assertEqual(
+            custom_filters.linkify('This is a short URL: https://example.com/PR911911.'),
+            mark_safe('This is a short URL: <a href="https://example.com/PR911911" target="_blank">https://example.com/PR911911</a>.')
+        )
+
+    def test_linkify_long_url_and_truncate(self):
+        self.assertEqual(
+            custom_filters.linkify('This is a long URL: https://example.com/some/really/long/url/that/needs/to/be/truncated/because/it/is/too/long.'),
+            mark_safe('This is a long URL: <a href="https://example.com/some/really/long/url/that/needs/to/be/truncated/because/it/is/too/long" target="_blank">https://example.com/...cause/it/is/too/long</a>.')
+        )
+
+    def test_linkify_with_long_url_and_feature_id(self):
+        self.assertEqual(
+            custom_filters.linkify('This is a long URL: https://example.com/some/really/long/url/with/feature/id/but/should/not/add/extra/link/CB011098.'),
+            mark_safe('This is a long URL: <a href="https://example.com/some/really/long/url/with/feature/id/but/should/not/add/extra/link/CB011098" target="_blank">https://example.com/.../extra/link/CB011098</a>.')
+        )
+
+    def test_linkify_with_long_url_and_feature_pr_id(self):
+        self.assertEqual(
+            custom_filters.linkify('This is a long URL: https://example.com/some/really/long/url/with/feature/and/pr/id/but/should/not/add/extra/link/PR123456/and/CB011098.'),
+            mark_safe('This is a long URL: <a href="https://example.com/some/really/long/url/with/feature/and/pr/id/but/should/not/add/extra/link/PR123456/and/CB011098" target="_blank">https://example.com/...R123456/and/CB011098</a>.')
+        )
 
     def test_linkify_no_match(self):
         input_text = "This string does not contain any PR numbers."
