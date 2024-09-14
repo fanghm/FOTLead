eae867bd04b09ad5b508c8a4ded21071b22b2038
Frank Fang <14533348@qq.com>
2024-09-14 17:32:49 +0800

    [#38] Update roughtime_since filter to resolve the "Text2 updated a few minutes ago but shown as xxx hours ago" issue temporarily

--- fotd/templatetags/custom_filters.py
+++ fotd/templatetags/custom_filters.py
@@ -1,100 +1,141 @@
-import re
 import datetime
+import re
+
 from django import template
 from django.utils.safestring import mark_safe
 from django.utils.timesince import timesince
 
 register = template.Library()
 
+
 @register.filter
 def replace(value, args):
-    old_value, new_value = args.split(',')
+    old_value, new_value = args.split(",")
     return value.replace(old_value, new_value)
-    
+
+
 @register.filter
 def startswith(text, starts):
     if isinstance(text, str):
         return text.startswith(starts)
     return False
 
-# when dict key is a variable: https://stackoverflow.com/questions/2894365/use-variable-as-dictionary-key-in-django-template
+
 @register.filter
 def keyvalue(dict, key):
-    return dict.get(key, '')
+    """Get dict value when dict key is a variable"""
+    return dict.get(key, "")
+
 
 @register.filter
 def get_previous_end_fb(endfb_changed_items, key):
-    return endfb_changed_items.get(key, {}).get('previous', 'blank') if endfb_changed_items else 'blank'
+    return (
+        endfb_changed_items.get(key, {}).get("previous", "blank")
+        if endfb_changed_items
+        else "blank"
+    )
+
 
 @register.filter
 def roughtime_since(value):
     if isinstance(value, datetime.date) and not isinstance(value, datetime.datetime):
         value = datetime.datetime.combine(value, datetime.datetime.min.time())
     elif not isinstance(value, datetime.datetime):
-        print(f'Not a datetime: {value}, it is of type {type(value)}')
-        return value
+        # print(f'Not a datetime: {value}, it is of type {type(value)}')
+        return "Unknown"
 
     time_diff = timesince(value)
-    #print(f'time_diff: {time_diff} ->{time_diff.split(",")[0]}')
+    # print(f'time_diff: {time_diff} ->{time_diff.split(",")[0]}')
 
-    # only keep the first time unit (day, week etc.) and remove the rest
-    return time_diff.split(',')[0]
+    if value.date() == datetime.datetime.now().date():
+        return "today"
+    else:
+        # only keep the first time unit (day, week etc.) and remove the rest
+        return time_diff.split(",")[0] + " ago"
 
-# Convert a plain text into html for matching patterns
-# Long URL will be truncated to use as the anchor text
-# Any PR or feature numbers, or Outlook names within an anchor text shouldn't be further transformed to break the link
+
 @register.filter
 def linkify(str):
+    """
+    Convert a plain text into html with links for matching patterns
+
+    Long URL will be truncated when using as anchor text
+    Any PR number, feature numbers, or Outlook names within an anchor text
+    shouldn't be further transformed to break the link
+    """
+
     # pattern to match normal URL and also a truncated URL with '...' in the middle
-    url_pattern = re.compile(r'https?://[^\s.]+(?:\.[^\s.]+)*(?:/[^\s]*)?(?:\.\.\.[^\s]*)?')
+    url_pattern = re.compile(
+        r"https?://[^\s.]+(?:\.[^\s.]+)*(?:/[^\s]*)?(?:\.\.\.[^\s]*)?"
+    )
 
     # patterns and the target html link to be replaced with
     non_url_patterns = {
-        r'PR\d{6}': 'https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText={}',
-        r'CB\d{6}-[CS]R': '/backlog/{}/',
-        r'CB\d{4,6}': '/backlog/{}/',
-        r'CNI-\d{6}': '/backlog/{}/',
-
-        # outlook name in the form of 'First-name Lastname (Nokia|NSB)', may have a number and middle name in between
-        r'\b[A-Z][a-z]+(?:-[A-Z][a-z]+)?(?: [A-Z]\.)?(?: \d{1,2}\.)? [A-Z][a-z]+ \((Nokia|NSB)\)': 'copy-to-clipboard:{}',
+        r"PR\d{6}": "https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText={}",  # NOQA
+        r"CB\d{6}-[CS]R": "/backlog/{}/",
+        r"CB\d{4,6}": "/backlog/{}/",
+        r"CNI-\d{6}": "/backlog/{}/",
+        # outlook display name in the form of 'Firstname Lastname (Nokia|NSB)',
+        # may have a number and middle name in between
+        r"\b[A-Z][a-z]+(?:-[A-Z][a-z]+)?(?: [A-Z]\.)?(?: \d{1,2}\.)? [A-Z][a-z]+ \((Nokia|NSB)\)": "copy-to-clipboard:{}",  # NOQA
     }
 
     def linkify_matches(match):
         for pattern, url in non_url_patterns.items():
             if re.fullmatch(pattern, match.group(0)):
-                if 'copy-to-clipboard' in url:
+                if "copy-to-clipboard" in url:
                     name = match.group(0)
-                    return f'<a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'{name}\'); alert(\'Name copied: {name}\'); return false;">{name}</a>'
+                    return (
+                        f'<a href="#copy-to-clipboard" '
+                        f"onclick=\"navigator.clipboard.writeText('{name}'); "
+                        f"alert('Name copied: {name}'); return false;\">{name}</a>"
+                    )
                 else:
-                    if pattern == r'CB\d{4,6}': # complement the feature number with 0s if it's incomplete
+                    # complement the feature number with 0s if it's incomplete
+                    if pattern == r"CB\d{4,6}":
                         cb_number = match.group(0)
-                        cb_number_padded = cb_number[:2] + cb_number[2:].zfill(6) + '-SR'
-                        return f'<a href="{url.format(cb_number_padded)}" target="_blank">{cb_number}</a>'
+                        cb_number_padded = (
+                            cb_number[:2] + cb_number[2:].zfill(6) + "-SR"
+                        )
+                        return (
+                            f'<a href="{url.format(cb_number_padded)}" '
+                            f'target="_blank">{cb_number}</a>'
+                        )
 
-                    return f'<a href="{url.format(match.group(0))}" target="_blank">{match.group(0)}</a>'
+                    return (
+                        f'<a href="{url.format(match.group(0))}" '
+                        f'target="_blank">{match.group(0)}</a>'
+                    )
         return match.group(0)
-        
+
     def linkify_per_patterns(match):
+        """Skip if it's a URL (or a truncated URL) to avoid further transformation"""
         if url_pattern.search(match.group(0)):
-            #print(f'Matched URL: {match.group(0)}')
-            return match.group(0)  # Skip if it's a URL (or a truncated URL) to avoid further transformation
+            # print(f'Matched URL: {match.group(0)}')
+            return match.group(0)
 
         return linkify_matches(match)
 
     def truncate_url_as_anchor(match):
         url = match.group(0)
         if len(url) > 50:
-            return f'{url[:20]}...{url[-20:]}'  # Truncate
+            return f"{url[:20]}...{url[-20:]}"  # Truncate
         return url
 
-    # First, convert URLs in the string into html links, and truncate long url as anchor text
-    urlized_str = url_pattern.sub(lambda match: f'<a href="{match.group(0)}" target="_blank">{truncate_url_as_anchor(match)}</a>', str)
-    #print(f'urlized_str: {urlized_str}')
+    # convert URLs in the string into html links, and truncate long url as anchor text
+    urlized_str = url_pattern.sub(
+        lambda match: (
+            f'<a href="{match.group(0)}" target="_blank">'
+            f"{truncate_url_as_anchor(match)}</a>"
+        ),
+        str,
+    )
+    # print(f'urlized_str: {urlized_str}')
 
     # Then, handle PR/feature numbers or names, but skip already processed URLs
-    combined_patterns = '|'.join(f'({pattern})' for pattern in non_url_patterns.keys())
-    combined_patterns = f'({url_pattern.pattern})|{combined_patterns}'
+    combined_patterns = "|".join(f"({pattern})" for pattern in non_url_patterns.keys())
+    combined_patterns = f"({url_pattern.pattern})|{combined_patterns}"
     regex = re.compile(combined_patterns)
     result = regex.sub(linkify_per_patterns, urlized_str)
-    
+
     return mark_safe(result)
--- fotd/tests/test_custom_filters.py
+++ fotd/tests/test_custom_filters.py
@@ -1,74 +1,118 @@
 from datetime import datetime, timedelta
+
 from django.test import TestCase
 from django.utils.safestring import SafeString, mark_safe
-from django.utils.timesince import timesince
+
 from fotd.templatetags import custom_filters
 
+
 class CustomFiltersTest(TestCase):
-	def test_replace(self):
-		self.assertEqual(custom_filters.replace('hello world', 'world,universe'), 'hello universe')
+    def test_replace(self):
+        self.assertEqual(
+            custom_filters.replace("hello world", "world,universe"), "hello universe"
+        )
 
-	def test_startswith(self):
-		self.assertTrue(custom_filters.startswith('hello world', 'hello'))
-		self.assertFalse(custom_filters.startswith('hello world', 'world'))
+    def test_startswith(self):
+        self.assertTrue(custom_filters.startswith("hello world", "hello"))
+        self.assertFalse(custom_filters.startswith("hello world", "world"))
 
-	def test_keyvalue(self):
-		self.assertEqual(custom_filters.keyvalue({'key': 'value'}, 'key'), 'value')
-		self.assertEqual(custom_filters.keyvalue({'key': 'value'}, 'nonexistent'), '')
+    def test_keyvalue(self):
+        self.assertEqual(custom_filters.keyvalue({"key": "value"}, "key"), "value")
+        self.assertEqual(custom_filters.keyvalue({"key": "value"}, "nonexistent"), "")
 
-	def test_get_previous_end_fb(self):
-		endfb_changed_items = {'item1': {'previous': 'value1'}}
-		self.assertEqual(custom_filters.get_previous_end_fb(endfb_changed_items, 'item1'), 'value1')
-		self.assertEqual(custom_filters.get_previous_end_fb(endfb_changed_items, 'item2'), 'blank')
+    def test_get_previous_end_fb(self):
+        endfb_changed_items = {"item1": {"previous": "value1"}}
+        self.assertEqual(
+            custom_filters.get_previous_end_fb(endfb_changed_items, "item1"), "value1"
+        )
+        self.assertEqual(
+            custom_filters.get_previous_end_fb(endfb_changed_items, "item2"), "blank"
+        )
+
 
 class RoughTimeSinceTests(TestCase):
     def _normalize_time_string(self, time_string):
-        return time_string.replace('\xa0', ' ') # replace non-breaking space '\xa0' with regular space
+        return time_string.replace(
+            "\xa0", " "
+        )  # replace non-breaking space '\xa0' with regular space
+
+    def test_roughtime_since_today(self):
+        now = datetime.now()
+        today = datetime(now.year, now.month, now.day, 0, 0, 1)
+        self.assertEqual(
+            self._normalize_time_string(custom_filters.roughtime_since(today)), "today"
+        )
+
+        just_now = now - timedelta(minutes=1)
+        self.assertEqual(
+            self._normalize_time_string(custom_filters.roughtime_since(just_now)),
+            "today",
+        )
+
+        just_now = now - timedelta(seconds=10)
+        self.assertEqual(
+            self._normalize_time_string(custom_filters.roughtime_since(just_now)),
+            "today",
+        )
 
     def test_roughtime_since_days(self):
         now = datetime.now()
         three_days_ago = now - timedelta(days=3)
-        result = self._normalize_time_string(custom_filters.roughtime_since(three_days_ago))
-        self.assertEqual(result, '3 days')
+        result = self._normalize_time_string(
+            custom_filters.roughtime_since(three_days_ago)
+        )
+        self.assertEqual(result, "3 days ago")
 
     def test_roughtime_since_weeks(self):
         now = datetime.now()
         two_weeks_ago = now - timedelta(weeks=2)
-        result = self._normalize_time_string(custom_filters.roughtime_since(two_weeks_ago))
-        self.assertEqual(result, '2 weeks')
+        result = self._normalize_time_string(
+            custom_filters.roughtime_since(two_weeks_ago)
+        )
+        self.assertEqual(result, "2 weeks ago")
 
     def test_roughtime_since_months(self):
         now = datetime.now()
-        two_months_ago = now - timedelta(days=70)   # 60 days -> 1 month, 4 weeks
-        result = self._normalize_time_string(custom_filters.roughtime_since(two_months_ago))
-        self.assertTrue('2 months' in result)
+        two_months_ago = now - timedelta(days=70)  # 60 days -> 1 month, 4 weeks
+        result = self._normalize_time_string(
+            custom_filters.roughtime_since(two_months_ago)
+        )
+        self.assertTrue("2 months" in result)
 
     def test_roughtime_since_years(self):
         now = datetime.now()
-        one_year_ago = now - timedelta(days=385)    # 365 days -> 11 months, 4 weeks
-        result = self._normalize_time_string(custom_filters.roughtime_since(one_year_ago))
-        self.assertTrue('1 year' in result)
+        one_year_ago = now - timedelta(days=385)  # 365 days -> 11 months, 4 weeks
+        result = self._normalize_time_string(
+            custom_filters.roughtime_since(one_year_ago)
+        )
+        self.assertTrue("1 year" in result)
 
     def test_roughtime_since_invalid_type(self):
-        self.assertEqual(custom_filters.roughtime_since("not a datetime"), "not a datetime")
+        self.assertEqual(custom_filters.roughtime_since("not a datetime"), "Unknown")
+        self.assertEqual(custom_filters.roughtime_since(None), "Unknown")
+
 
 class LinkifyTests(TestCase):
     def test_linkify_pr(self):
         input_text = "Here is a PR number: PR123456, and one more: PR654321."
         expected_output = (
-            'Here is a PR number: '
-            '<a href="https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText=PR123456" target="_blank">PR123456</a>'
-            ', and one more: '
-            '<a href="https://pronto.ext.net.nokia.com/pronto/problemReportSearch.html?freeTextdropDownID=prId&searchTopText=PR654321" target="_blank">PR654321</a>.'
+            "Here is a PR number: "
+            '<a href="https://pronto.ext.net.nokia.com/pronto/'
+            'problemReportSearch.html?freeTextdropDownID=prId&searchTopText=PR123456" '
+            'target="_blank">PR123456</a>'
+            ", and one more: "
+            '<a href="https://pronto.ext.net.nokia.com/pronto/'
+            'problemReportSearch.html?freeTextdropDownID=prId&searchTopText=PR654321" '
+            'target="_blank">PR654321</a>.'
         )
         result = custom_filters.linkify(input_text)
         self.assertIsInstance(result, SafeString)
         self.assertEqual(result, expected_output)
 
     def test_linkify_feature_id(self):
-        input_text = "The FOTL is taking care of feature CB8701, CB123456, CB012842-CR and CNI-654321."
+        input_text = "I'm working on CB8701, CB123456, CB012842-CR and CNI-654321."
         expected_output = (
-            'The FOTL is taking care of feature '
+            "I'm working on "
             '<a href="/backlog/CB008701-SR/" target="_blank">CB8701</a>'
             ', <a href="/backlog/CB123456-SR/" target="_blank">CB123456</a>'
             ', <a href="/backlog/CB012842-CR/" target="_blank">CB012842-CR</a>'
@@ -84,13 +128,16 @@ class LinkifyTests(TestCase):
             "name_with_number": "Hua 77. Zhao (NSB)",
             "name_with_middle_name": "Mask M. Mike (Nokia)",
         }
-    
+
         for name_type, name in test_names.items():
-            with self.subTest(test_name=f'test_linkify_{name_type}'):
+            with self.subTest(test_name=f"test_linkify_{name_type}"):
                 input_text = f"Contact: {name}."
                 expected_output = (
-                    'Contact: '
-                    f'<a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'{name}\'); alert(\'Name copied: {name}\'); return false;">{name}</a>.'
+                    "Contact: "
+                    f'<a href="#copy-to-clipboard" '
+                    f"onclick=\"navigator.clipboard.writeText('{name}'); "
+                    f"alert('Name copied: {name}'); return false;\">"
+                    f"{name}</a>."
                 )
                 result = custom_filters.linkify(input_text)
                 self.assertIsInstance(result, SafeString)
@@ -99,8 +146,12 @@ class LinkifyTests(TestCase):
     def test_linkify_normal_name_and_feature_id(self):
         input_text = "Waiting feedback from CB9999 FOTL Frankson Smith (Nokia)"
         expected_output = (
-            'Waiting feedback from '
-            '<a href="/backlog/CB009999-SR/" target="_blank">CB9999</a> FOTL <a href="#copy-to-clipboard" onclick="navigator.clipboard.writeText(\'Frankson Smith (Nokia)\'); alert(\'Name copied: Frankson Smith (Nokia)\'); return false;">Frankson Smith (Nokia)</a>'
+            "Waiting feedback from "
+            '<a href="/backlog/CB009999-SR/" target="_blank">CB9999</a> FOTL '
+            '<a href="#copy-to-clipboard" '
+            "onclick=\"navigator.clipboard.writeText('Frankson Smith (Nokia)'); "
+            "alert('Name copied: Frankson Smith (Nokia)'); return false;\">"
+            "Frankson Smith (Nokia)</a>"
         )
         result = custom_filters.linkify(input_text)
         self.assertIsInstance(result, SafeString)
@@ -108,44 +159,97 @@ class LinkifyTests(TestCase):
 
     def test_linkify_short_url(self):
         self.assertEqual(
-            custom_filters.linkify('This is a short URL: https://example.com/short.'),
-            mark_safe('This is a short URL: <a href="https://example.com/short" target="_blank">https://example.com/short</a>.')
+            custom_filters.linkify("This is a short URL: https://example.com/short."),
+            mark_safe(
+                "This is a short URL: "
+                '<a href="https://example.com/short" target="_blank">'
+                "https://example.com/short</a>."
+            ),
         )
 
     def test_linkify_short_url2(self):
         self.assertEqual(
-            custom_filters.linkify('Combined PGO knife is ready at: https://es-si-s3-z2.eecloud.nsn-net.net/knife-results/1231165/s3_packages_list.html'),
-            mark_safe('Combined PGO knife is ready at: <a href="https://es-si-s3-z2.eecloud.nsn-net.net/knife-results/1231165/s3_packages_list.html" target="_blank">https://es-si-s3-z2....3_packages_list.html</a>')
+            custom_filters.linkify(
+                "Combined PGO knife is ready at: "
+                "https://es-si-s3-z2.eecloud.nsn-net.net/knife-results"
+                "/1231165/s3_packages_list.html"
+            ),
+            mark_safe(
+                "Combined PGO knife is ready at: "
+                '<a href="https://es-si-s3-z2.eecloud.nsn-net.net/knife-results'
+                '/1231165/s3_packages_list.html" target="_blank">'
+                "https://es-si-s3-z2....3_packages_list.html"
+                "</a>"
+            ),
         )
 
     def test_linkify_short_url_with_feature_id(self):
         self.assertEqual(
-            custom_filters.linkify('This is a short URL: http://example.com/short/feature/CB11098/.'),
-            mark_safe('This is a short URL: <a href="http://example.com/short/feature/CB11098/" target="_blank">http://example.com/short/feature/CB11098/</a>.')
+            custom_filters.linkify(
+                "This is a short URL: http://example.com/short/feature/CB11098/."
+            ),
+            mark_safe(
+                "This is a short URL: "
+                '<a href="http://example.com/short/feature/CB11098/" target="_blank">'
+                "http://example.com/short/feature/CB11098/"
+                "</a>."
+            ),
         )
 
     def test_linkify_short_url_with_pr_id(self):
         self.assertEqual(
-            custom_filters.linkify('This is a short URL: https://example.com/PR911911.'),
-            mark_safe('This is a short URL: <a href="https://example.com/PR911911" target="_blank">https://example.com/PR911911</a>.')
+            custom_filters.linkify(
+                "This is a short URL: https://example.com/PR911911."
+            ),
+            mark_safe(
+                "This is a short URL: "
+                '<a href="https://example.com/PR911911" target="_blank">'
+                "https://example.com/PR911911</a>."
+            ),
         )
 
     def test_linkify_long_url_and_truncate(self):
         self.assertEqual(
-            custom_filters.linkify('This is a long URL: https://example.com/some/really/long/url/that/needs/to/be/truncated/because/it/is/too/long.'),
-            mark_safe('This is a long URL: <a href="https://example.com/some/really/long/url/that/needs/to/be/truncated/because/it/is/too/long" target="_blank">https://example.com/...cause/it/is/too/long</a>.')
+            custom_filters.linkify(
+                "This is a long URL: https://example.com/some/really/long/url/that/"
+                "needs/to/be/truncated/because/it/is/too/long."
+            ),
+            mark_safe(
+                "This is a long URL: "
+                '<a href="https://example.com/some/really/long/url/that/needs/to/'
+                'be/truncated/because/it/is/too/long" target="_blank">'
+                "https://example.com/...cause/it/is/too/long</a>."
+            ),
         )
 
     def test_linkify_with_long_url_and_feature_id(self):
         self.assertEqual(
-            custom_filters.linkify('This is a long URL: https://example.com/some/really/long/url/with/feature/id/but/should/not/add/extra/link/CB011098.'),
-            mark_safe('This is a long URL: <a href="https://example.com/some/really/long/url/with/feature/id/but/should/not/add/extra/link/CB011098" target="_blank">https://example.com/.../extra/link/CB011098</a>.')
+            custom_filters.linkify(
+                "This is a long URL: https://example.com/some/really/long/url/with/"
+                "feature/id/but/should/not/add/extra/link/CB011098."
+            ),
+            mark_safe(
+                "This is a long URL: "
+                '<a href="https://example.com/some/really/long/url/with/feature/id/'
+                'but/should/not/add/extra/link/CB011098" target="_blank">'
+                "https://example.com/.../extra/link/CB011098</a>."
+            ),
         )
 
     def test_linkify_with_long_url_and_feature_pr_id(self):
         self.assertEqual(
-            custom_filters.linkify('This is a long URL: https://example.com/some/really/long/url/with/feature/and/pr/id/but/should/not/add/extra/link/PR123456/and/CB011098.'),
-            mark_safe('This is a long URL: <a href="https://example.com/some/really/long/url/with/feature/and/pr/id/but/should/not/add/extra/link/PR123456/and/CB011098" target="_blank">https://example.com/...R123456/and/CB011098</a>.')
+            custom_filters.linkify(
+                "This is a long URL: https://example.com/some/really/long/url/with/"
+                "feature/and/pr/id/but/should/not/add/extra/link/PR123456/and/"
+                "CB011098."
+            ),
+            mark_safe(
+                "This is a long URL: "
+                '<a href="https://example.com/some/really/long/url/with/feature/and/'
+                'pr/id/but/should/not/add/extra/link/PR123456/and/CB011098" '
+                'target="_blank">'
+                "https://example.com/...R123456/and/CB011098</a>."
+            ),
         )
 
     def test_linkify_no_match(self):
