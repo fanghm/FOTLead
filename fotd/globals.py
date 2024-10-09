1f7d14de42784334fbc7c71b06611a9ff72579f3
Frank Fang <14533348@qq.com>
2024-10-09 11:00:31 +0800

    [#51] fix bug: when fb not defined in db, _get_fb_start_date() returns None and cause exception
    when fb not defined in db, deduce the fb dates

--- fotd/globals.py
+++ fotd/globals.py
@@ -1,19 +1,26 @@
 # globals.py
-from django.core.cache import cache
+import datetime
 from collections import namedtuple
-from django.db.models import QuerySet
+
+from django.core.cache import cache
+
 from .models import Sprint
 
 CACHE_TIMEOUT = 3600 * 24  # 24 hours
 SprintDates = namedtuple('SprintDates', ['start_date', 'end_date'])
 
+
 def _query_database_for_fb_dict():
     print("Querying database for FB data")
     sprints = Sprint.objects.values('fb', 'start_date', 'end_date')
-    fb_dict = {sprint['fb']: SprintDates(sprint['start_date'], sprint['end_date']) for sprint in sprints}
-    
+    fb_dict = {
+        sprint['fb']: SprintDates(sprint['start_date'], sprint['end_date'])
+        for sprint in sprints
+    }
+
     print(f'FB dict size: {len(fb_dict)}')
     return fb_dict
+
 
 def _get_fb_dict():
     fb_dict = cache.get('fb_dict')
@@ -23,13 +30,46 @@ def _get_fb_dict():
 
     return fb_dict
 
+
+def _deduce_fb_date(fb, start=True):
+    if not fb.startswith('FB'):
+        fb = 'FB' + fb
+
+    year = int('20' + fb[2:4])
+    fb_number = int(fb[4:6])
+
+    # FBs start on the first Wednesday of the year
+    base_date = datetime.date(year, 1, 1)
+    while base_date.weekday() != 2:  # 0 = Monday, 1 = Tuesday, ..., 6 = Sunday
+        base_date += datetime.timedelta(days=1)
+
+    days_offset = (fb_number - 1) * 14
+
+    if start:
+        guessed_date = base_date + datetime.timedelta(days=days_offset)
+    else:
+        guessed_date = base_date + datetime.timedelta(days=days_offset + 13)
+
+    return guessed_date
+
+
 def _get_fb_start_date(fb):
     if not fb.startswith('FB'):
         fb = 'FB' + fb
-    return _get_fb_dict()[fb].start_date if fb in _get_fb_dict() else None
-    
+
+    if fb in _get_fb_dict():
+        return _get_fb_dict()[fb].start_date
+    else:
+        print(f'Warning: FB {fb} not found in db cache')
+        return _deduce_fb_date(fb, start=True)
+
 
 def _get_fb_end_date(fb):
     if not fb.startswith('FB'):
         fb = 'FB' + fb
-    return _get_fb_dict()[fb].end_date if fb in _get_fb_dict() else None
+
+    if fb in _get_fb_dict():
+        return _get_fb_dict()[fb].end_date
+    else:
+        print(f'Warning: FB {fb} not found in db cache')
+        return _deduce_fb_date(fb, start=False)
--- fotd/tests/test_globals.py
+++ fotd/tests/test_globals.py
@@ -1,42 +1,74 @@
 # test_globals.py
-from django.test import TestCase
-from django.core.cache import cache
-from fotd.models import Sprint
-from fotd.globals import _get_fb_start_date, _get_fb_end_date, CACHE_TIMEOUT
 import datetime
+import time
+
+from django.core.cache import cache
+from django.test import TestCase
+
+from fotd.globals import _deduce_fb_date, _get_fb_end_date, _get_fb_start_date
+from fotd.models import Sprint
+
 
 class GlobalsTestCase(TestCase):
     def setUp(self):
         cache.clear()
-        
+
         # prepare test data
-        Sprint.objects.create(fb='FB2301', start_date=datetime.date(2023, 1, 4), end_date=datetime.date(2023, 1, 17))
-        Sprint.objects.create(fb='FB2302', start_date=datetime.date(2023, 1, 18), end_date=datetime.date(2023, 1, 31))
-        Sprint.objects.create(fb='FB2303', start_date=datetime.date(2023, 2, 1), end_date=datetime.date(2023, 2, 14))
+        Sprint.objects.create(
+            fb='FB2301',
+            start_date=datetime.date(2023, 1, 4),
+            end_date=datetime.date(2023, 1, 17),
+        )
+        Sprint.objects.create(
+            fb='FB2302',
+            start_date=datetime.date(2023, 1, 18),
+            end_date=datetime.date(2023, 1, 31),
+        )
+        Sprint.objects.create(
+            fb='FB2303',
+            start_date=datetime.date(2023, 2, 1),
+            end_date=datetime.date(2023, 2, 14),
+        )
+
+    def test_deduce_start_date(self):
+        self.assertEqual(_deduce_fb_date('FB2501', True), datetime.date(2025, 1, 1))
+        self.assertEqual(_deduce_fb_date('FB2502', True), datetime.date(2025, 1, 15))
+        self.assertEqual(_deduce_fb_date('FB2503', True), datetime.date(2025, 1, 29))
+        self.assertEqual(_deduce_fb_date('FB2504', True), datetime.date(2025, 2, 12))
+
+    def test_deduce_end_date(self):
+        self.assertEqual(_deduce_fb_date('FB2501', False), datetime.date(2025, 1, 14))
+        self.assertEqual(_deduce_fb_date('FB2502', False), datetime.date(2025, 1, 28))
+        self.assertEqual(_deduce_fb_date('FB2503', False), datetime.date(2025, 2, 11))
+        self.assertEqual(_deduce_fb_date('FB2504', False), datetime.date(2025, 2, 25))
 
     def test_get_fb_start_date(self):
         self.assertEqual(_get_fb_start_date('FB2301'), datetime.date(2023, 1, 4))
         self.assertEqual(_get_fb_start_date('FB2302'), datetime.date(2023, 1, 18))
         self.assertEqual(_get_fb_start_date('FB2303'), datetime.date(2023, 2, 1))
 
-        # test getting start date for non-existent FB
-        self.assertIsNone(_get_fb_start_date('FB9999'))
+        # test getting start date for not defined FB
+        self.assertIsNotNone(_get_fb_start_date('FB2801'))
 
     def test_get_fb_end_date(self):
         self.assertEqual(_get_fb_end_date('FB2301'), datetime.date(2023, 1, 17))
         self.assertEqual(_get_fb_end_date('FB2302'), datetime.date(2023, 1, 31))
         self.assertEqual(_get_fb_end_date('FB2303'), datetime.date(2023, 2, 14))
 
-        # test getting end date for non-existent FB
-        self.assertIsNone(_get_fb_end_date('FB9999'))
+        # test getting end date for not defined FB
+        self.assertIsNotNone(_get_fb_end_date('FB2926'))
 
     def test_cache(self):
         _get_fb_start_date('FB2301')  # trigger cache set
         self.assertIsNotNone(cache.get('fb_dict'))
 
         # test cache timeout
-        cache.set('fb_dict', {'FB2301': (datetime.date(2023, 1, 4), datetime.date(2023, 1, 17))}, timeout=1)
+        cache.set(
+            'fb_dict',
+            {'FB2301': (datetime.date(2023, 1, 4), datetime.date(2023, 1, 17))},
+            timeout=1,
+        )
+
         self.assertIsNotNone(cache.get('fb_dict'))
-        import time
         time.sleep(2)
         self.assertIsNone(cache.get('fb_dict'))
